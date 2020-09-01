e2280f39b9989c1964a4b44d373f6402e49452d0
live-git <crafted.coded.2003@gmail.com>
2020-09-01 12:23:23 +0530

    .

---
+++ ld-java-master/src/com/live/jdbc/JDBCSelect.java
@@ -0,0 +1,41 @@
+package com.live.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCSelect {
+	public static void main(String[] args) {
+		// CHECKED exceptions : compiler force checks that certain exceptions are
+		// handled
+		// Step 1 : Load the driver
+
+		try {
+			//Deprecated
+//			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");			
+			System.out.println("Successfully loaded the driver!");
+			//Step 2: Create the connection
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila","root","mysql");
+			System.out.println("Successfully connected to the database!");
+			//Step 3: Create a statement (sql query)
+			Statement statement = connection.createStatement();
+			//Step 4: Create the query
+			String sqlActorQuery = "select actor_id, first_name from actor where actor_id < 6";
+			//Step 5: Execute Query and get the ResultSet
+			ResultSet resultSet = statement.executeQuery(sqlActorQuery);
+			while(resultSet.next()) {
+				int actorId = resultSet.getInt("actor_id");
+				String firstNameString = resultSet.getString("first_name");
+				System.out.println(actorId + " " + firstNameString);
+			}
+		} catch (ClassNotFoundException e) {
+			System.out.println("The driver class was not found : " + e);
+		} catch (SQLException e) {
+			System.out.println("The server could not be connected due to n/w or credentials or server errors!");
+		}
+
+	}
+}
