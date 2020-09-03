e988d685d8d9361e5d6e1ff41f5a0685fc600892
live-git <crafted.coded.2003@gmail.com>
2020-09-03 11:44:38 +0530

    .

--- spring/demos/le1c-springboot-rest-get-post/src/main/java/com/live/model/Product.java
+++ spring/demos/le1c-springboot-rest-get-post/src/main/java/com/live/model/Product.java
@@ -5,42 +5,54 @@ public class Product {
 	private String productName;
 	private String productDescription;
 	private float price;
+
 	public Product(long productId, String productName, String productDescription, float price) {
-		super();
+		System.err.println("public Product(long productId, String productName, String productDescription, float price) ");
 		this.productId = productId;
 		this.productName = productName;
 		this.productDescription = productDescription;
 		this.price = price;
 	}
-	public Product() {
-		super();
-	}
+	
+	/*
+	 * public Product() { System.err.println(" Product()"); }
+	 */
+	 
 	@Override
 	public String toString() {
 		return "Product [productId=" + productId + ", productName=" + productName + ", productDescription="
 				+ productDescription + ", price=" + price + "]";
 	}
+
 	public Long getProductId() {
 		return productId;
 	}
+
 	public void setProductId(Long productId) {
+		System.out.println("public void setProductId(Long productId)");
 		this.productId = productId;
 	}
+
 	public String getProductName() {
 		return productName;
 	}
+
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
+
 	public String getProductDescription() {
 		return productDescription;
 	}
+
 	public void setProductDescription(String productDescription) {
 		this.productDescription = productDescription;
 	}
+
 	public float getPrice() {
 		return price;
 	}
+
 	public void setPrice(float price) {
 		this.price = price;
 	}
